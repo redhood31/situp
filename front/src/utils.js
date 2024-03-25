7aba9f6b3f3d61c7d31413aeca348602b0db43dc
redhood31 <wolf@wolf>
2024-03-25 07:23:48 +0100

    fixed dependecis and env added

--- front/src/utils.js
+++ front/src/utils.js
@@ -1,6 +1,8 @@
 
 // export const BACKEND = ' https://c6e8-195-150-224-33.ngrok-free.app'
 // export const WEBSOCKET = 'wss://c6e8-195-150-224-33.ngrok-free.app'
-export const BACKEND = 'http://localhost:8000'
-export const WEBSOCKET = 'ws://localhost:8000'
+export const BACKEND = process.env.REACT_APP_BACKEND
+export const WEBSOCKET = process.env.REACT_APP_WEBSOCKET
+
+console.log("BACKEND IS ", BACKEND)
 // ws://
